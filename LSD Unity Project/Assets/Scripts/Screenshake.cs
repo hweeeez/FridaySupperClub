c847145fa890235ad21dd30e4d0e9ac00778c894
hweeeez <89247473+hweeeez@users.noreply.github.com>
2022-01-24 14:09:03 +0800

    Basic Screenshake Script

---
+++ LSD Unity Project/Assets/Scripts/Screenshake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class Screenshake
+{
+    public static IEnumerator ShakeCoroutine(Camera inCamera, float inMagnitude, float inDuration)
+    {
+        float elapsed = 0;
+        Vector3 originalPos = inCamera.transform.localPosition;
+        do
+        {
+            elapsed += Time.deltaTime;
+            inCamera.transform.localPosition = originalPos + Random.insideUnitSphere * inMagnitude;
+            yield return null;
+        }
+        while (elapsed < inDuration);
+        inCamera.transform.localPosition = originalPos;
+    }
+}
