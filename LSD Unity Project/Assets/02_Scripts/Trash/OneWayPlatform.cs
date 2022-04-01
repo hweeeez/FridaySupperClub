345f326db15290a4448981b01b4c8eedebcdbf34
tamago04 <ljyhinako@gmail.com>
2022-04-01 17:53:31 +0800

    [Scene] one way platform WIP

---
+++ LSD Unity Project/Assets/02_Scripts/Trash/OneWayPlatform.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))] //will auto add boxcollider when you attach this script to smth
+public class OneWayPlatform : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 entryDirection = Vector3.up; //direction the platform needs to be entered from
+    [SerializeField] private bool localDirection = false; //want to know if this direction is a local or global direction
+    [SerializeField, Range(1.0f, 2.0f)] private float triggerScale = 2f;
+    private new BoxCollider collider = null; //overrides old deprecated variable
+
+    public LayerMask ground;
+
+    private BoxCollider collisionCheckTrigger = null;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider>();
+        collider.isTrigger = false;
+
+        //this collider will detect if player is close to the object + direction player is coming from
+        collisionCheckTrigger = gameObject.AddComponent<BoxCollider>(); //adds a new box collider
+        collisionCheckTrigger.size = collider.size * triggerScale;
+        collisionCheckTrigger.center = collider.center;
+        collisionCheckTrigger.isTrigger = true;
+
+        Physics.IgnoreLayerCollision(ground, ground, true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print(transform.rotation + " " + other.transform.rotation);
+        if (Physics.ComputePenetration(
+            collisionCheckTrigger, transform.position, transform.rotation,
+            other, other.transform.position, other.transform.rotation,
+            out Vector3 collisionDirection, out float penetrationDepth
+            )) //will simulate the collision and will return true if collision happens
+        {
+
+            Vector3 direction;
+            if (localDirection)
+            {
+                direction = transform.TransformDirection(entryDirection.normalized);
+            }
+            else
+            {
+                direction = entryDirection;
+            }
+
+            //if it happens, we want to check the diff between entryDirection and actual direction the player is coming from
+            float dot = Vector3.Dot(direction, collisionDirection); //dot < 0, means they're facing opposite ways = coming from wrong direction
+
+            //Opposite direction, passing is not allowed
+            if (dot < 0)
+            {
+                Physics.IgnoreCollision(collider, other, false);
+            }
+            else //when dot > 0
+            {
+                Physics.IgnoreCollision(collider, other, true);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 direction;
+        if (localDirection)
+        {
+            direction = transform.TransformDirection(entryDirection.normalized);
+        }
+        else
+        {
+            direction = entryDirection;
+        }
+        //so that when localDirection enabled, when you rotate the entry direction will change too!
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, direction);
+        //Gizmos.DrawRay(transform.position, entryDirection);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, -direction);
+        //Gizmos.DrawRay(transform.position, -entryDirection);
+    }
+
+}
